da3a4fcfd3ef4698cd8545f372b0a7c5878e0ad8
khehhin <khehhin@yahoo.com>
2023-01-06 12:45:01 +0800

    Fixe some code based on lighthouse

--- web/test.php
+++ web/test.php
@@ -1,5 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
+        <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- fix : Document does not have a meta description -->
+  <meta name="description" content="Description of the web page.">
+  <!-- fix : Document doesn't have a <title> element -->
+
+        <title> Heroku PHP Web App </title>
 </head>
 <body>
     Hello World
