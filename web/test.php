32c0bbdffcef9e36c18e4b6a62244bd08956ade1
khehhin <khehhin@yahoo.com>
2023-01-06 15:07:13 +0800

    Reverted back to plain html

--- web/test.php
+++ web/test.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- fix : Document does not have a meta description -->
-  <meta name="description" content="Description of the web page.">
-  <!-- fix : Document doesn't have a <title> element -->
-
-        <title> Heroku PHP Web App </title>
 </head>
 <body>
     Hello World
